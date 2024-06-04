42e7a9bfb685c087e531408535b96936d356868b
7vTr0x7 <7vTrox7@gmail.com>
2024-06-04 11:54:13 +0530

    IR1.1_HW1

--- IR1.1_HW1/src/App.jsx
+++ IR1.1_HW1/src/App.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+const Heading = () => {
+  return <h1>React Playground</h1>;
+};
+
+const Greetings = () => {
+  const name = "Alice Smith";
+  return <h1>Hello, {name}!</h1>;
+};
+
+const ExternalLink = () => {
+  const url = "https://www.reactexamples.com";
+  return (
+    <a href={url} target="_blank">
+      Visit React Examples
+    </a>
+  );
+};
+const Avatar = () => {
+  const imgURL = "https://via.placeholder.com/200";
+  return (
+    <div>
+      <img src={imgURL} alt="avatar" />
+    </div>
+  );
+};
+
+const UserInfo = () => {
+  const user = {
+    name: "Alice Smith",
+
+    age: 28,
+
+    email: "alice.smith@example.com",
+
+    bio: "Passionate about React and building innovative web applications.",
+  };
+
+  return (
+    <div>
+      <h2>User Information</h2>
+      <p>
+        <b>Name:</b> {user.name}
+      </p>
+      <p>
+        <b>Age: </b> {user.age}
+      </p>
+      <p>
+        <b>Email: </b> {user.email}
+      </p>
+      <p>
+        <b>Bio: </b> {user.bio}
+      </p>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Heading />
+      <Greetings />
+      <ExternalLink />
+      <Avatar />
+      <UserInfo />
+    </div>
+  );
 };
 
 export default App;

3b7c6d081f45c64d0ab5c13e845fc43b82850581
7vTr0x7 <7vTrox7@gmail.com>
2024-06-05 11:31:50 +0530

    IR1.2_CW

--- IR1.2_CW/src/App.jsx
+++ IR1.2_CW/src/App.jsx
@@ -1,7 +1,128 @@
-import React from "react";
+const StudentDetails = () => {
+  const details = {
+    name: "Bob",
+    age: 20,
+    grade: "A",
+  };
+
+  return (
+    <div>
+      <h2>Name: {details.name}</h2>
+      <p>
+        <b>Age: </b>
+        {details.age}
+      </p>
+      <p>
+        <b>Grade: </b>
+        {details.grade}
+      </p>
+    </div>
+  );
+};
+
+const Product = () => {
+  const details = {
+    name: "Laptop",
+    brand: "Dell",
+    price: 20000,
+  };
+
+  return (
+    <div>
+      <p>
+        <b>Name: </b>
+        {details.name}
+      </p>
+      <p>
+        <b>Brand: </b>
+        {details.brand}
+      </p>
+      <p>
+        <b>Price: </b>INR {details.price}
+      </p>
+    </div>
+  );
+};
+
+const UserDetails = () => {
+  const details = {
+    name: "John Doe",
+    age: 30,
+    address: {
+      street: "123 main",
+      city: "New York",
+      state: "CA",
+      zipcode: "234543",
+    },
+  };
+
+  return (
+    <div>
+      <p>
+        <b>Name: </b>
+        {details.name}
+      </p>
+      <p>
+        <b>Age: </b>
+        {details.age}
+      </p>
+      <p>
+        <b>Address: </b>
+        {details.address.street}, {details.address.city},{" "}
+        {details.address.state}, {details.address.zipcode}
+      </p>
+    </div>
+  );
+};
+
+const MovieDetails = () => {
+  const details = {
+    title: "Inception",
+    releaseYear: 2010,
+    director: {
+      name: "Christopher Nolan",
+      nationality: "British",
+    },
+    genre: ["Sci-Fi", "Action", "Thriller"],
+  };
+
+  return (
+    <div>
+      <p>
+        <b>Title: </b>
+        {details.title}
+      </p>
+      <p>
+        <b>Release Year: </b>
+        {details.releaseYear}
+      </p>
+      <p>
+        <b>Director: </b>
+        {details.director.name} ({details.director.nationality})
+      </p>
+      <p>
+        <b>Genre: </b>
+        {details.genre.join(", ")}
+      </p>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Student Details</h1>
+      <StudentDetails />
+
+      <h1>Product Details</h1>
+      <Product />
+
+      <h1>User Details</h1>
+      <UserDetails />
+      <h1>Movie Details</h1>
+      <MovieDetails />
+    </div>
+  );
 };
 
 export default App;

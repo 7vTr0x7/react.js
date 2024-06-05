8c18e7e4c498257f20ed93763024b0ea76da458d
7vTr0x7 <7vTrox7@gmail.com>
2024-06-05 14:35:49 +0530

    IR1.3_HW1

--- IR1.3_CW/src/App.jsx
+++ IR1.3_CW/src/App.jsx
@@ -1,46 +1,7 @@
-const Greeting = (props) => {
-  return <h1>Hello, {props.name}</h1>;
-};
-
-const UserDetails = (props) => {
-  return (
-    <div>
-      <h1>User Details</h1>
-      <p>Name: {props.name}</p>
-      <p>Age: {props.age}</p>
-      <p>Country: {props.country}</p>
-    </div>
-  );
-};
-
-const WeatherInfo = (props) => {
-  return (
-    <div>
-      <h1>Weather Info</h1>
-      <p>Temperature: {props.temperature} degree celsius</p>
-      <p>Condition: {props.condition}</p>
-    </div>
-  );
-};
-
-const UserAvatar = (props) => {
-  return (
-    <div>
-      <h1>User Avatar</h1>
-      <img src={props.img} alt={props.alt} />
-    </div>
-  );
-};
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <Greeting name="John" />
-      <UserDetails name="Bob" age={20} country="India" />
-      <WeatherInfo temperature={29} condition="Sunny" />
-      <UserAvatar img="https://via.placeholder.com/150" alt="avatar" />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;

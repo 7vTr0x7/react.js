6a538049c6056cfdcdc1e04dbe38f2c6d8c74273
7vTr0x7 <7vTrox7@gmail.com>
2024-06-06 16:16:37 +0530

    IR1.7_HW2

--- IR1.7_HW2/src/App.jsx
+++ IR1.7_HW2/src/App.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+const CreditCardPayment = ({ amount }) => (
+  <p>
+    {amount > 1000
+      ? "Please use your credit card for payment."
+      : "You can pay using cash or debit card."}
+  </p>
+);
+
+const ProductAvailability = ({ inStock }) => (
+  <p>
+    {inStock
+      ? "The product is available. Order now!"
+      : "Sorry, the product is currently out of stock."}
+  </p>
+);
+
+const TrafficLight = ({ color }) => (
+  <p>
+    {color === "red" ? "Stop! The light is red." : "Go! The light is green."}
+  </p>
+);
+
+const MembershipStatus = ({ isMember }) => (
+  <p>
+    {isMember
+      ? "Welcome, Premium Member!"
+      : "Upgrade to premium for exclusive benefits."}
+  </p>
+);
+
+const WeatherReport = ({ isSunny }) => (
+  <p>{isSunny ? "It is sunny today." : "It is not sunny today."}</p>
+);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CreditCardPayment amount={2000} />
+      <ProductAvailability inStock={true} />
+      <TrafficLight color="red" />
+      <MembershipStatus isMember={false} />
+      <WeatherReport isSunny={false} />
+    </div>
+  );
 };
 
 export default App;

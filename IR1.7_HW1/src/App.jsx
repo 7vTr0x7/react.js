d67595b0bac5bed5bc9607ee81dd14f36c78e3ce
7vTr0x7 <7vTrox7@gmail.com>
2024-06-06 16:02:51 +0530

    IR1.7_HW1

--- IR1.7_HW1/src/App.jsx
+++ IR1.7_HW1/src/App.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+const UserStatus = ({ isOnline }) => (
+  <p>{isOnline ? "User is online" : "User is offline"}</p>
+);
+
+const DayOfWeek = ({ isWeekend }) => (
+  <p>{isWeekend ? "Enjoy your weekend!" : "Have a great day!"}</p>
+);
+
+const ShoppingCart = ({ itemCount }) => (
+  <p>
+    {itemCount > 0
+      ? `You have ${itemCount} items in your cart`
+      : "Your cart is empty"}
+  </p>
+);
+
+const DiscountOffer = ({ isPrimeMember }) => (
+  <p>
+    {isPrimeMember
+      ? "You're eligible for a 20% discount"
+      : "Become a prime member for exclusive discounts"}
+  </p>
+);
+
+const ShowError = ({ showError }) => (
+  <div>{showError && <p>{showError}</p>}</div>
+);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <UserStatus isOnline={true} />
+      <DayOfWeek isWeekend={true} />
+      <ShoppingCart itemCount={7} />
+      <DiscountOffer isPrimeMember={false} />
+      <ShowError showError="An error occurred. Please try again later." />
+    </div>
+  );
 };
 
 export default App;

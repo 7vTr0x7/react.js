1ddb95f9a494dee5ce211d520396334b4afb7c48
7vTr0x7 <7vTrox7@gmail.com>
2024-06-06 12:11:23 +0530

    IR1.6_CW

--- IR1.6_CW/src/App.jsx
+++ IR1.6_CW/src/App.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+const CallGreetingFunction = () => {
+  const getGreeting = () => {
+    return "Hello World!";
+  };
+
+  return <h1>{getGreeting()}</h1>;
+};
+
+const MathOperation = () => {
+  const a = 20;
+  const b = 8;
+
+  const add = (x, y) => {
+    return x + y;
+  };
+  const sub = (x, y) => {
+    return x - y;
+  };
+
+  return (
+    <div>
+      <p>
+        {a} + {b} equals {add(a, b)}
+      </p>
+      <p>
+        {a} - {b} equals {sub(a, b)}
+      </p>
+    </div>
+  );
+};
+
+const GetFormattedDate = () => {
+  const getDate = () => {
+    return new Date().toLocaleDateString();
+  };
+
+  return <p>Current Date: {getDate()}</p>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <CallGreetingFunction />
+      <MathOperation />
+      <GetFormattedDate />
+    </main>
+  );
 };
 
 export default App;

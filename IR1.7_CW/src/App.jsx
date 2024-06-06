e03c9944c8ea73f6f7e118d17684d6bd6d1c7fc2
7vTr0x7 <7vTrox7@gmail.com>
2024-06-06 15:40:28 +0530

    IR1.7_CW

--- IR1.7_CW/src/App.jsx
+++ IR1.7_CW/src/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+const Message = ({ isLoggedIn }) => (
+  <p>{isLoggedIn ? "Welcome back, User" : "Please login to continue"}</p>
+);
+
+const Greeting = ({ isMorning }) => (
+  <p>{isMorning ? "Good Morning" : "Good Evening"}</p>
+);
+
+const ShowNotification = ({ notification }) => (
+  <div>{notification && <p>{notification}</p>}</div>
+);
+
+const TicketPricing = ({ passengerAge }) => {
+  const price = 100;
+  return (
+    <div>
+      <p>Ticket Price: {passengerAge > 60 ? price - price * 0.15 : price}</p>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Message isLoggedIn={true} />
+      <Greeting isMorning={true} />
+      <ShowNotification notification="Notification Message" />
+      <TicketPricing passengerAge={50} />
+    </div>
+  );
 };
 
 export default App;

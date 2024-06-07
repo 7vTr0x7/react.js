6e9198e0254906f580bc21aa8e03130b42b3b4e5
7vTr0x7 <7vTrox7@gmail.com>
2024-06-07 17:34:53 +0530

    IR2.3_CW

--- IR2.3_CW/src/App.jsx
+++ IR2.3_CW/src/App.jsx
@@ -1,4 +1,38 @@
-import React from "react";
+const ProductList = ({ products }) => {
+  const totalPrice = products.reduce((acc, curr) => acc + curr.price, 0);
+
+  return (
+    <div>
+      <h2>Total Price of Products</h2>${totalPrice}
+    </div>
+  );
+};
+
+const RestaurantsList = ({ restaurants }) => {
+  const totalRating = restaurants.reduce((acc, curr) => acc + curr.rating, 0);
+
+  const avgRating = totalRating / restaurants.length;
+
+  return (
+    <div>
+      <h2>Total Rating of Restaurants</h2>
+      <p>{totalRating}</p>
+      <h2>Average Rating of Restaurants</h2>
+      <p>{avgRating.toFixed(2)}</p>
+    </div>
+  );
+};
+
+const Video = ({ videos }) => {
+  const totalViews = videos.reduce((acc, curr) => acc + curr.views, 0);
+
+  return (
+    <div>
+      <h2>Total Views of Videos</h2>
+      <p>{totalViews}</p>
+    </div>
+  );
+};
 
 const App = () => {
   const products = [
@@ -7,9 +41,9 @@ const App = () => {
     { id: 3, name: "Product 3", price: 39.99 },
   ];
   const restaurants = [
-    { id: 1, name: "Restaurant 1", cuisine: "Italian" },
-    { id: 2, name: "Restaurant 2", cuisine: "Mexican" },
-    { id: 3, name: "Restaurant 3", cuisine: "Chinese" },
+    { id: 1, name: "Restaurant 1", cuisine: "Italian", rating: 5 },
+    { id: 2, name: "Restaurant 2", cuisine: "Mexican", rating: 3.5 },
+    { id: 3, name: "Restaurant 3", cuisine: "Chinese", rating: 2.7 },
   ];
 
   const videos = [
@@ -18,7 +52,15 @@ const App = () => {
     { id: 3, title: "Video 3", views: 3000 },
   ];
 
-  return <div>App</div>;
+  return (
+    <div>
+      <ProductList products={products} />
+      <hr />
+      <RestaurantsList restaurants={restaurants} />
+      <hr />
+      <Video videos={videos} />
+    </div>
+  );
 };
 
 export default App;

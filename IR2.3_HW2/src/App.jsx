d6833e0eb91bf176bba3a573d3771de58499707d
7vTr0x7 <7vTrox7@gmail.com>
2024-06-07 19:41:12 +0530

    IR2.3_HW2

--- IR2.3_HW2/src/App.jsx
+++ IR2.3_HW2/src/App.jsx
@@ -42,6 +42,61 @@ const Cafes = ({ cafes }) => {
   );
 };
 
+const Books = ({ books }) => {
+  const totalPages = books.reduce((acc, curr) => acc + curr.pages, 0);
+  return (
+    <div>
+      <h2>Total Number of Pages</h2>
+      <p>{totalPages}</p>
+    </div>
+  );
+};
+
+const Tweets = ({ tweets }) => {
+  const filterMorningTweets = tweets.filter(
+    (tweet) => tweet.schedule === "Morning tweet"
+  );
+  const filterEveningTweets = tweets.filter(
+    (tweet) => tweet.schedule === "Evening tweet"
+  );
+
+  const totalMorningTweetLikes = filterMorningTweets.reduce(
+    (acc, curr) => acc + curr.likes,
+    0
+  );
+  const totalEveningTweetLikes = filterEveningTweets.reduce(
+    (acc, curr) => acc + curr.likes,
+    0
+  );
+
+  return (
+    <div>
+      <h2>Morning Tweet Like Count</h2>
+      <p>{totalMorningTweetLikes}</p>
+      <h2>Evening Tweet Like Count</h2>
+      <p>{totalEveningTweetLikes}</p>
+    </div>
+  );
+};
+
+const Podcast = ({ podcasts }) => {
+  const filterPodcast = podcasts.filter(
+    (podcast) => podcast.type === "verified"
+  );
+
+  const totalListeners = filterPodcast.reduce(
+    (acc, curr) => acc + curr.listeners,
+    0
+  );
+
+  return (
+    <div>
+      <h2>Total Verified Listeners</h2>
+      <p>{totalListeners}</p>
+    </div>
+  );
+};
+
 const App = () => {
   const blogPosts = [
     {
@@ -86,6 +141,31 @@ const App = () => {
     { id: 3, name: "Me and U", rating: 4.3, type: "restaurant" },
   ];
 
+  const books = [
+    { id: 1, title: "Book 1", pages: 300 },
+
+    { id: 2, title: "Book 2", pages: 250 },
+
+    { id: 3, title: "Book 3", pages: 400 },
+  ];
+
+  const tweets = [
+    { id: 1, content: "Tweet 1", likes: 20, schedule: "Morning tweet" },
+
+    { id: 2, content: "Tweet 2", likes: 15, schedule: "Evening tweet" },
+
+    { id: 3, content: "Tweet 3", likes: 30, schedule: "Morning tweet" },
+
+    { id: 4, content: "Tweet 4", likes: 15, schedule: "Evening tweet" },
+  ];
+
+  const podcasts = [
+    { id: 1, title: "Tech Talk", listeners: 5000, type: "verified" },
+
+    { id: 2, title: "Science Insights", listeners: 1000, type: "unverified" },
+
+    { id: 3, title: "Comedy Hour", listeners: 7000, type: "verified" },
+  ];
   return (
     <div>
       <BlogPosts blogPosts={blogPosts} />
@@ -93,6 +173,12 @@ const App = () => {
       <Laptops laptops={laptops} />
       <hr />
       <Cafes cafes={cafes} />
+      <hr />
+      <Books books={books} />
+      <hr />
+      <Tweets tweets={tweets} />
+      <hr />
+      <Podcast podcasts={podcasts} />
     </div>
   );
 };

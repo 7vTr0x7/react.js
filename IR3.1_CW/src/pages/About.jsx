c1aa2eeba3110fba9ec7c2c93a1725728f1ed39a
7vTr0x7 <7vTrox7@gmail.com>
2024-06-11 18:55:51 +0530

    IR3.1_CW - routes

--- IR3.1_CW/src/pages/About.jsx
+++ IR3.1_CW/src/pages/About.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const About = () => {
   return (
-    <main>
+    <main className="container py-4">
       <h1>About Us</h1>
       <p>
-        {" "}
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and

9e70f1bfb2c1fc28dc513bd60ece324c5c25b832
7vTr0x7 <7vTrox7@gmail.com>
2024-06-12 15:56:34 +0530

    IR3.2_HW1 - Header

--- IR3.2_HW1/src/App.jsx
+++ IR3.2_HW1/src/App.jsx
@@ -1,32 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <main className="container py-4">
-      <h1 className="display-5">Featured Restaurants</h1>
-      <div>
+    <>
+      <Header />
+      <main className="container py-4">
+        <h1 className="display-5">Featured Restaurants</h1>
         <div>
-          <img
-            src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            className="img-fluid"
-            alt="restaurant 1"
-          />
-          <h3 className="py-2">Taste of Italy</h3>
-          <p>Italian | Rating: 4.5</p>
-          <p>123 main street, Anytown, USA</p>
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="img-fluid"
+              alt="restaurant 1"
+            />
+            <h3 className="py-2">Taste of Italy</h3>
+            <p>Italian | Rating: 4.5</p>
+            <p>123 main street, Anytown, USA</p>
+          </div>
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="img-fluid"
+              alt="restaurant 2"
+            />
+            <h3 className="py-2">Spice Village</h3>
+            <p>Indian | Rating: 4.3</p>
+            <p>456 Elm street, Anytown, USA</p>
+          </div>
         </div>
-        <div>
-          <img
-            src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            className="img-fluid"
-            alt="restaurant 2"
-          />
-          <h3 className="py-2">Spice Village</h3>
-          <p>Indian | Rating: 4.3</p>
-          <p>456 Elm street, Anytown, USA</p>
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
--- IR3.2_HW1/src/pages/About.jsx
+++ IR3.2_HW1/src/pages/About.jsx
@@ -1,37 +1,42 @@
+import Header from "../components/Header";
+
 const About = () => {
   return (
-    <main className="container py-4">
-      <h1>About Our Restaurants</h1>
-      <div>
-        <p>
-          Welcome to our restaurant, where culinary artistry meets warm
-          hospitality. Nestled in the heart of downtown, our restaurant is a
-          celebration of exquisite flavors, meticulously crafted dishes, and an
-          ambiance that transports you to a realm of gastronomic delight. With a
-          passion for both tradition and innovation, we strive to offer an
-          unforgettable dining experience that tantalizes the senses and leaves
-          a lasting impression.
-        </p>
-        <p>
-          At our restaurant, every dish is a masterpiece, created with the
-          finest ingredients sourced locally and globally. Our talented team of
-          chefs brings together a symphony of flavors from around the world,
-          infusing each recipe with creativity and expertise. Whether you crave
-          the comforting familiarity of classic favorites or the thrill of
-          culinary exploration, our menu is designed to satisfy every palate and
-          ignite the spirit of adventure.
-        </p>
-        <p>
-          Beyond the delectable fare, our restaurant is a haven for connection
-          and community. Whether you're gathering with loved ones for a special
-          occasion, enjoying a romantic evening for two, or simply seeking a
-          respite from the bustle of the city, our warm and welcoming atmosphere
-          invites you to relax, indulge, and create cherished memories. Come
-          join us on a journey of culinary discovery, where every meal is an
-          opportunity to savor life's simple pleasures.
-        </p>
-      </div>
-    </main>
+    <>
+      <Header />
+      <main className="container py-4">
+        <h1>About Our Restaurants</h1>
+        <div>
+          <p>
+            Welcome to our restaurant, where culinary artistry meets warm
+            hospitality. Nestled in the heart of downtown, our restaurant is a
+            celebration of exquisite flavors, meticulously crafted dishes, and
+            an ambiance that transports you to a realm of gastronomic delight.
+            With a passion for both tradition and innovation, we strive to offer
+            an unforgettable dining experience that tantalizes the senses and
+            leaves a lasting impression.
+          </p>
+          <p>
+            At our restaurant, every dish is a masterpiece, created with the
+            finest ingredients sourced locally and globally. Our talented team
+            of chefs brings together a symphony of flavors from around the
+            world, infusing each recipe with creativity and expertise. Whether
+            you crave the comforting familiarity of classic favorites or the
+            thrill of culinary exploration, our menu is designed to satisfy
+            every palate and ignite the spirit of adventure.
+          </p>
+          <p>
+            Beyond the delectable fare, our restaurant is a haven for connection
+            and community. Whether you're gathering with loved ones for a
+            special occasion, enjoying a romantic evening for two, or simply
+            seeking a respite from the bustle of the city, our warm and
+            welcoming atmosphere invites you to relax, indulge, and create
+            cherished memories. Come join us on a journey of culinary discovery,
+            where every meal is an opportunity to savor life's simple pleasures.
+          </p>
+        </div>
+      </main>
+    </>
   );
 };
 

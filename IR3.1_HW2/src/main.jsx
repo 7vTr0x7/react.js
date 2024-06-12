7f5b42644ceb0fd4a4a304d09df46ba30b3ac7ed
7vTr0x7 <7vTrox7@gmail.com>
2024-06-12 12:37:03 +0530

    IR3.1_HW2 -routes

--- IR3.1_HW2/src/main.jsx
+++ IR3.1_HW2/src/main.jsx
@@ -2,8 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import About from "./pages/About.jsx";
+import Stays from "./pages/Stays.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/stays",
+    element: <Stays />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

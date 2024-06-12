28b82cbe3cebd3daed47b6801a725ef4612be641
7vTr0x7 <7vTrox7@gmail.com>
2024-06-12 15:41:33 +0530

    IR3.2_CW Footer

--- IR3.2_CW/src/App.jsx
+++ IR3.2_CW/src/App.jsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -48,6 +49,7 @@ const App = () => {
           </p>
         </article>
       </main>
+      <Footer />
     </>
   );
 };

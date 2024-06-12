259241949d6b15f49eb17d0cb02548a683c98d42
7vTr0x7 <7vTrox7@gmail.com>
2024-06-12 11:50:59 +0530

    IR3.1_HW1 - components and routes

--- IR3.1_HW1/src/main.jsx
+++ IR3.1_HW1/src/main.jsx
@@ -2,8 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import About from "./pages/About.jsx";
+import Restaurants from "./pages/Restaurants.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+  {
+    path: "/Restaurants",
+    element: <Restaurants />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

ce44ae3daad3a3d0d6c183024753686c978a5d43
7vTr0x7 <7vTrox7@gmail.com>
2024-06-13 16:42:33 +0530

    IR3_Assignment1 - components and pages

--- IR3_Assignment1/src/App.jsx
+++ IR3_Assignment1/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <h1 className="fs-1 fw-normal">Featured Products</h1>
+      </main>
+    </>
+  );
 };
 
 export default App;
--- IR3_Assignment1/src/components/Header.jsx
+++ IR3_Assignment1/src/components/Header.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-dark text-light">
+      <div className="container pt-4">
+        <h1>Ecommerce Website</h1>
+        <nav className="pt-4">
+          <ul className="nav">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">Products</li>
+            <li className="nav-item">About</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

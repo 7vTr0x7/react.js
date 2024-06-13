a325f0b870911b745b65191ae4fbec1ea7dc7c98
7vTr0x7 <7vTrox7@gmail.com>
2024-06-13 16:51:10 +0530

    IR3_Assignment1 - Header routes

--- IR3_Assignment1/src/components/Header.jsx
+++ IR3_Assignment1/src/components/Header.jsx
@@ -7,9 +7,21 @@ const Header = () => {
         <h1>Ecommerce Website</h1>
         <nav className="pt-4">
           <ul className="nav">
-            <li className="nav-item">Home</li>
-            <li className="nav-item">Products</li>
-            <li className="nav-item">About</li>
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/products" className="nav-link">
+                Products
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                About
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>

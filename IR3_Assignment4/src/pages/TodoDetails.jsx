936f7811d1e8c879e6fd3fe5fbd28b7aad49f390
7vTr0x7 <7vTrox7@gmail.com>
2024-06-14 13:19:37 +0530

    IR3_Assignment4 - TodoDetails

--- IR3_Assignment4/src/pages/TodoDetails.jsx
+++ IR3_Assignment4/src/pages/TodoDetails.jsx
@@ -1,5 +1,30 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+import { todos } from "../utility/common";
+import { useParams } from "react-router-dom";
+
 const TodoDetails = () => {
-  return <div>TodoDetails</div>;
+  const todoId = useParams();
+
+  const todoData = todos.find((todo) => todo.id == todoId.todoId);
+
+  return (
+    <>
+      <Header />
+      <main className="container py-4">
+        <div className="col-md-6">
+          <div className="card p-3">
+            <h2>Todo Details</h2>
+            <p>ID: {todoData.id}</p>
+            <p>Title: {todoData.title}</p>
+            <p>Description: {todoData.description}</p>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default TodoDetails;

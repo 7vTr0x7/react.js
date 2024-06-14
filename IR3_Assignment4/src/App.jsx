1b154f11c1a099d40c864e7d04a923ccdeaf0305
7vTr0x7 <7vTrox7@gmail.com>
2024-06-14 12:59:14 +0530

    IR3_Assignment4 - App

--- IR3_Assignment4/src/App.jsx
+++ IR3_Assignment4/src/App.jsx
@@ -1,12 +1,30 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main></main>
+      <main className="container py-4">
+        <div className="text-center">
+          <img
+            alt="todoImg"
+            className="img-Fluid"
+            src="https://via.placeholder.com/500x350"
+          />
+          <div className="py-4">
+            <h2>Todos</h2>
+            <p>Welcome to Todo application</p>
+            <p>
+              {/* <Link to="/todos" className="btn btn-primary">
+                View todos
+              </Link> */}
+            </p>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   );

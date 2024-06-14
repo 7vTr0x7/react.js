9c599e8ea37bb38f7734304327ac6a10ce0ebc13
7vTr0x7 <7vTrox7@gmail.com>
2024-06-14 11:52:43 +0530

    IR3_Assignment3 - routes

--- IR3_Assignment3/src/main.jsx
+++ IR3_Assignment3/src/main.jsx
@@ -2,8 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from "./pages/Profile.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );

b491e6ddb1287fe6d7ee8fc6a7c9e03279063516
7vTr0x7 <7vTrox7@gmail.com>
2024-06-14 11:47:54 +0530

    IR3_Assignment3 - Footer

--- IR3_Assignment3/src/App.jsx
+++ IR3_Assignment3/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -6,6 +7,7 @@ const App = () => {
     <>
       <Header />
       <main></main>;
+      <Footer />
     </>
   );
 };

0b8dc5889dd1ccacd8b980e4386e9f3c967ce4fc
7vTr0x7 <7vTrox7@gmail.com>
2024-06-14 11:50:47 +0530

    IR3_Assignment3 - Profile

---
+++ IR3_Assignment3/src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const Profile = () => {
+  return (
+    <>
+      <Header />
+      <main></main>;
+      <Footer />
+    </>
+  );
+};
+
+export default Profile;

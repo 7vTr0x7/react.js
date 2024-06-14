de23bbf23d2fb365906b6f59e99ddb9452ab2075
7vTr0x7 <7vTrox7@gmail.com>
2024-06-14 13:06:02 +0530

    IR3_Assignment4 - Header  Routes

--- IR3_Assignment4/src/components/Header.jsx
+++ IR3_Assignment4/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="bg-dark text-light">
@@ -5,9 +7,21 @@ const Header = () => {
         <h1 className="fs-1 fw-normal">Todos</h1>
         <nav className="pt-4">
           <ul className="nav">
-            <li className="nav-item">Home</li>
-            <li className="nav-item">Todos</li>
-            <li className="nav-item">About</li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/todos" className="nav-link">
+                Todos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
